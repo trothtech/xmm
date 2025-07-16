1b83a24841c20e032856b624f02673bea83d24d6
Rick Troth <rick@trothtech.us>
2025-07-16 11:04:17 -0400

    2.1.9

--- src/debbuild.sh
+++ src/debbuild.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
 #         Name: debbuild.sh (shell script)
-#               build a .deb package file from Debian "control"
+#               build a .deb package file from Debian "control" hierarchy
 #         Date: 2025-07-14 (Mon)
 #     See also: rpmbuild.sh script in this package
 #
 
-#PREFIX                 # set from $STAGING                            +
+#PREFIX                 # set from $STAGING
 #APPLID                 # taken from makefile, supplied as argument $1
 #VERSION                # taken from makefile, supplied as argument $2
 #UNAMEM                 # derived from `uname -m`
@@ -14,15 +14,12 @@
 
 # run from the resident directory
 cd `dirname "$0"`
-D=`pwd`
 
-# I wish the following two were not hard-coded
-
+# two arguments from the makefile
 APPLID="$1"
 if [ -z "$APPLID" ] ; then echo "debbuild: missing APPLID"
     echo "debbuild: you're doing it wrong, drive this from 'make'"
     exit 1 ; fi
-
 VERSION="$2"
 if [ -z "$VERSION" ] ; then echo "rdebbuild missing VERSION"
     echo "debbuild: you're doing it wrong, drive this from 'make'"
@@ -30,8 +27,7 @@ if [ -z "$VERSION" ] ; then echo "rdebbuild missing VERSION"
 
 STAGING=`pwd`/debbuild.d
 
-#
-UNAMEM=`uname -m`
+# establish certain variables
 UNAMEM=`uname -m | sed 's#^i.86$#i386#' | sed 's#^armv.l$#arm#'`
 # Debian fixups
 if [ "$UNAMEM" = "x86_64" ] ; then UNAMEM=amd64 ; fi
@@ -51,21 +47,16 @@ $MAKE STAGING=$STAGING UNAMEM=$UNAMEM RELEASE=$RELEASE $APPLID.dctl
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
 #
-# clean up from any prior run
-$MAKE clean 1> /dev/null 2> /dev/null
-rm -rf $STAGING
-#find . -print | grep ';' | xargs -r rm
-
-#
 # configure the package normally
-#./configure --prefix=/usr
-./configure --prefix=$PREFIX
+./configure # --prefix=$PREFIX
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
 #
-# 'just make'
-$MAKE
+# 'just make' - build all deliverables
+$MAKE all # just short of doing 'make install'
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
+$MAKE rexx 2> /dev/null
+$MAKE java 2> /dev/null
 
 #
 # override the PREFIX for the install step
@@ -74,11 +65,19 @@ RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
 #
 # make it "properly rooted"
-mkdir -p $STAGING
+mkdir -p $STAGING$PREFIX
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
-mkdir $STAGING$PREFIX
-mv $STAGING/*bin $STAGING/lib* $STAGING/share $STAGING$PREFIX/.
-RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
+
+#mv $STAGING/*bin $STAGING/lib* $STAGING/share $STAGING$PREFIX/.
+#RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
+
+for D in bin lib lib64 libexec sbin share include ; do
+    mv $STAGING/$D $STAGING$PREFIX/. 2> /dev/null
+done
+if [ -d $STAGING/src ] ; then
+    mkdir -p $STAGING$PREFIX/src
+    mv $STAGING/src $STAGING$PREFIX/src/$APPLID
+fi
 
 #
 # move the control file into place
@@ -93,13 +92,12 @@ rm -f $APPLID.deb.log
 echo "+ dpkg-deb --build debbuild.d"
         dpkg-deb --build debbuild.d 2>&1 | tee $APPLID.deb.log
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
-rm $APPLID.dctl
 
 #
 # recover the  resulting package file ... yay!
-mv debbuild.d.deb $APPLID.deb
+mv debbuild.d.deb $APPLID-$VERSION-$UNAMEM.deb
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
-cp -p $APPLID.deb $APPLID-$VERSION-$UNAMEM.deb
+cp -p $APPLID-$VERSION-$UNAMEM.deb $APPLID.deb
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
 #
@@ -107,5 +105,13 @@ RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 rm -r $STAGING
 
 exit
+
+########################################################################
+
+#
+# clean up from any prior run
+$MAKE clean 1> /dev/null 2> /dev/null
+rm -rf $STAGING
+#find . -print | grep ';' | xargs -r rm
 
 
--- src/rpmbuild.sh
+++ src/rpmbuild.sh
@@ -4,50 +4,91 @@
 #               build an RPM from the "spec file"
 #         Date: 2023-03-22 (Wed)
 #
+#
+
+#PREFIX                 # set from $STAGING
+#APPLID                 # taken from makefile, supplied as argument $1
+#VERSION                # taken from makefile, supplied as argument $2
+#UNAMEM                 # derived from `uname -m`
+#STAGING                # (see below)
 
 # run from the resident directory
 cd `dirname "$0"`
 
+# two arguments from the makefile
+APPLID="$1"
+if [ -z "$APPLID" ] ; then echo "rpmbuild: missing APPLID"
+    echo "rpmbuild: you're doing it wrong, drive this from 'make'"
+    exit 1 ; fi
+VERSION="$2"
+if [ -z "$VERSION" ] ; then echo "rpmbuild: missing VERSION"
+    echo "rpmbuild: you're doing it wrong, drive this from 'make'"
+    exit 1 ; fi
+
+STAGING=`pwd`/rpmbuild.d
+
 # establish certain variables
 UNAMEM=`uname -m | sed 's#^i.86$#i386#' | sed 's#^armv.l$#arm#'`
+# RPM fixups
 if [ ! -s .rpmseq ] ; then echo "0" > .rpmseq ; fi
 RELEASE=`cat .rpmseq`
-STAGING=`pwd`/rpmbuild.d
-export UNAMEM RELEASE STAGING
+export UNAMEM STAGING RELEASE
 
-# I wish the following were not hard-coded
-APPLID=$1
-if [ -z "$APPLID" ] ; then echo "missing APPLID - you're doing it wrong, drive this from 'make'" ; exit 1 ; fi
-VERSION=$2
-if [ -z "$VERSION" ] ; then echo "missing VERSION - you're doing it wrong, drive this from 'make'" ; exit 1 ; fi
+#
+# we're moving more settings into the config artifacts
+. ./configure.sh
+# CFLAGS, PREFIX, SYSTEM, LDFLAGS, SHFLAGS, LOCDIR, LOCALE
+if [ -z "$MAKE" ] ; then MAKE=make ; fi
 
+#
+# process the skeletal spec file into a usable RPM spec file
+rm -f $APPLID.spec
+$MAKE STAGING=$STAGING UNAMEM=$UNAMEM RELEASE=$RELEASE $APPLID.spec
+RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
+
+#
 # configure the package normally
-./configure
+./configure # --prefix=$PREFIX
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
-# build all deliverables
-make all # just short of doing 'make install'
+# 
+# 'just make' - build all deliverables
+$MAKE all # just short of doing 'make install'
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
-make rexx 2> /dev/null
-make java 2> /dev/null
+$MAKE rexx 2> /dev/null
+$MAKE java 2> /dev/null
 
+#
 # override the PREFIX for the install step
-make PREFIX=$STAGING install
+$MAKE PREFIX=$STAGING install
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
-# remake the spec file
-rm -f xmitmsgx.spec
-make STAGING=$STAGING UNAMEM=$UNAMEM RELEASE=$RELEASE xmitmsgx.spec
+#
+# make it "properly rooted"
+mkdir -p $STAGING$PREFIX
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
+#mv $STAGING/*bin $STAGING/lib* $STAGING/share $STAGING$PREFIX/.
+#RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
+
+for D in bin lib lib64 libexec sbin share include ; do
+    mv $STAGING/$D $STAGING$PREFIX/. 2> /dev/null
+done
+if [ -d $STAGING/src ] ; then
+    mkdir -p $STAGING$PREFIX/src
+    mv $STAGING/src $STAGING$PREFIX/src/$APPLID
+fi
+
+#
 # dump the heavy lifting on the 'rpmbuild' command
-rpmbuild -bb --nodeps xmitmsgx.spec
+rpmbuild -bb --nodeps $APPLID.spec
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
 
+#
 # recover the resulting package file ... yay!
 mv $HOME/rpmbuild/RPMS/$UNAMEM/$APPLID-$VERSION-$RELEASE.$UNAMEM.rpm .
 RC=$? ; if [ $RC -ne 0 ] ; then exit $RC ; fi
-cp -p $APPLID-$VERSION-$RELEASE.$UNAMEM.rpm xmitmsgx.rpm
+cp -p $APPLID-$VERSION-$RELEASE.$UNAMEM.rpm $APPLID.rpm
 
 # increment the sequence number for the next build
 expr $RELEASE + 1 > .rpmseq
